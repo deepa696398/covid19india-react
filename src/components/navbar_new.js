1399ed34d111e6c52776e17ee2ea5ab025fa7ecc
jeremyphilemon <jeremyphilemon@outlook.com>
2020-04-12 08:37:14 +0300

    Added navbar expansion.

--- src/components/navbar_new.js
+++ src/components/navbar_new.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 function Navbar(props) {
+  const [expand, setExpand] = useState(false);
+
   return (
     <div className="Navbar">
       <div className="navbar-left">English</div>
@@ -10,7 +12,19 @@ function Navbar(props) {
           Covid19<span>India</span>
         </Link>
       </div>
-      <div className="navbar-right">Menu</div>
+      <div
+        className="navbar-right"
+        style={{
+          background: expand ? '#4c75f2' : '',
+          color: expand ? 'white' : '',
+        }}
+        onClick={() => {
+          setExpand(!expand);
+        }}
+      >
+        {expand ? 'Close' : 'Menu'}
+      </div>
+      {expand && <div className="expand"></div>}
     </div>
   );
 }
